10f940ccb9d6c3a6ef57f78dcd5b8f6e26519c19
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-24 15:30:06 -0500

    Added path of defaults write.

--- manifests/darwin/s1_3.pp
+++ manifests/darwin/s1_3.pp
@@ -4,6 +4,7 @@
 class cis_config::darwin::s1_3 {
   exec { 'cis-1.3':
     command => 'defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool TRUE',
+    path    => '/usr/bin'
     unless  => "defaults read /Library/Preferences/com.apple.commerce AutoUpdate | grep '1'"
     }
 }
