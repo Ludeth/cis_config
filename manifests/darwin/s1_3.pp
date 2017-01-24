194c13f92088cb1893e43046dc7ecb3991a246fe
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-24 16:02:21 -0500

    Corrected unless path

--- manifests/darwin/s1_3.pp
+++ manifests/darwin/s1_3.pp
@@ -5,6 +5,6 @@ class cis_config::darwin::s1_3 {
   exec { 'cis-1.3':
     command => 'defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool TRUE',
     path    => '/usr/bin'
-    unless  => "defaults read /Library/Preferences/com.apple.commerce AutoUpdate | grep '1'"
+    unless  => "/usr/bin/defaults read /Library/Preferences/com.apple.commerce AutoUpdate | grep '1'"
     }
 }
