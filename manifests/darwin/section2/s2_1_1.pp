35c916d32fc1aac12c6e6510b2396128e24be7fa
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-24 16:52:30 -0500

    Added FQ path to unless

--- manifests/darwin/section2/s2_1_1.pp
+++ manifests/darwin/section2/s2_1_1.pp
@@ -3,6 +3,6 @@
 class cis_config::darwin::section2::s2_1_1 {
   exec { 'bluetooth-device-check':
     command => '/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0',
-    unless  => "system_profiler SPBluetoothDataType | grep 'Connected: Yes'"
+    unless  => "/usr/sbin/system_profiler SPBluetoothDataType | grep 'Connected: Yes'"
   }
 }
