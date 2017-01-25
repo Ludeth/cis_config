3027abb62db45f89d24cbacc4c636e0cfc39307f
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-25 15:02:07 -0500

    More idempodence checking

--- manifests/darwin/section2/s2_1_1.pp
+++ manifests/darwin/section2/s2_1_1.pp
@@ -3,11 +3,11 @@
 class cis_config::darwin::section2::s2_1_1 {
   exec { 'bluetooth-device-check':
     command => '/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0',
-    unless  => "/usr/sbin/system_profiler SPBluetoothDataType | grep -q 'Bluetooth Power: On'"
+    unless  => '/usr/bin/defaults read /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0'
   }
   exec { 'bluetooth-disable-if-none':
     command   => '/usr/bin/killall -HUP blued',
-    subscribe => bluetooth-device-check,
+    subscribe => 'bluetooth-device-check',
     unless    => "/usr/sbin/system_profiler SPBluetoothDataType | grep -q 'Connected: Yes'"
   }
 }
