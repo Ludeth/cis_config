18af86c17179bd3f4b850d9913eb8b854d5412b5
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-24 22:43:13 -0500

    Added s2_1_3

---
+++ manifests/darwin/section2/s2_1_3.pp
@@ -0,0 +1,8 @@
+# == Class: cis_config::darwin::section2::s2_1_3
+#
+class cis_config::darwin::section2::s2_1_3 {
+  exec { 'ensure-bluetooth-menu-item':
+    command => '/usr/bin/defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"',
+    unless  => 'defaults read com.apple.systemuiserver menuExtras | grep Bluetooth.menu'
+  }
+}
