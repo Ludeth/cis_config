5d521482ba99a6b64504fb946f3ed8e273a6812e
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-25 14:15:17 -0500

    Edit to grep to return true/false

--- manifests/darwin/section2/s2_1_1.pp
+++ manifests/darwin/section2/s2_1_1.pp
@@ -3,10 +3,10 @@
 class cis_config::darwin::section2::s2_1_1 {
   exec { 'bluetooth-device-check':
     command => '/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0',
-    unless  => "/usr/sbin/system_profiler SPBluetoothDataType | grep 'Connected: Yes'"
+    unless  => "/usr/sbin/system_profiler SPBluetoothDataType | grep -q 'Connected: Yes'"
   }
   exec { 'bluetooth-disable-if-none':
     command => '/usr/bin/killall -HUP blued',
-    unless  => "/usr/sbin/system_profiler SPBluetoothDataType | grep 'Connected: Yes'"
+    unless  => "/usr/sbin/system_profiler SPBluetoothDataType | grep -q 'Connected: Yes'"
   }
 }
