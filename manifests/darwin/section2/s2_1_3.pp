2ca77591c3c61cb7cae91b4de9b2499ba05839ac
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-25 14:17:14 -0500

    Added true/false for additional functions.

--- manifests/darwin/section2/s2_1_3.pp
+++ manifests/darwin/section2/s2_1_3.pp
@@ -3,7 +3,7 @@
 class cis_config::darwin::section2::s2_1_3 {
   exec { 'ensure-bluetooth-menu-item':
     command => '/usr/bin/defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"',
-    unless  => '/usr/bin/defaults read com.apple.systemuiserver menuExtras | grep Bluetooth.menu',
+    unless  => '/usr/bin/defaults read com.apple.systemuiserver menuExtras | grep -q Bluetooth.menu',
     user    => $::current_user
   }
 }
