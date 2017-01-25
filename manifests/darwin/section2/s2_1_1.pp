c917cbba4de412a1d6da3c43c04a3a7e671c8443
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-24 21:58:19 -0500

    Added the killall blued section

--- manifests/darwin/section2/s2_1_1.pp
+++ manifests/darwin/section2/s2_1_1.pp
@@ -5,4 +5,8 @@ class cis_config::darwin::section2::s2_1_1 {
     command => '/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0',
     unless  => "/usr/sbin/system_profiler SPBluetoothDataType | grep 'Connected: Yes'"
   }
+  exec { 'bluetooth-disable-if-none':
+    command => '/usr/bin/killall -HUP blued',
+    unless  => "/usr/sbin/system_profiler SPBluetoothDataType | grep 'Connected: Yes'"
+  }
 }
