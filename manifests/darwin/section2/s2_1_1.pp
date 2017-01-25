1bcee680d07ced239bc20c3343c1b1c09212b6ca
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-25 15:14:42 -0500

    Correcting refresh_only condition

--- manifests/darwin/section2/s2_1_1.pp
+++ manifests/darwin/section2/s2_1_1.pp
@@ -6,8 +6,9 @@ class cis_config::darwin::section2::s2_1_1 {
     unless  => '/usr/bin/defaults read /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0'
   }
   exec { 'bluetooth-disable-if-none':
-    command   => '/usr/bin/killall -HUP blued',
-    unless    => "/usr/sbin/system_profiler SPBluetoothDataType | grep -q 'Connected: Yes'",
-    subscribe => Exec['bluetooth-device-check'],
+    command     => '/usr/bin/killall -HUP blued',
+    unless      => "/usr/sbin/system_profiler SPBluetoothDataType | grep -q 'Connected: Yes'",
+    refreshonly => true,
+    subscribe   => Exec['bluetooth-device-check']
   }
 }
