a588fad6d0897719ef9c871bb65946c04d26001f
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-25 15:24:15 -0500

    Logic correction

--- manifests/darwin/section2/s2_1_1.pp
+++ manifests/darwin/section2/s2_1_1.pp
@@ -3,7 +3,8 @@
 class cis_config::darwin::section2::s2_1_1 {
   exec { 'bluetooth-device-check':
     command => '/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0',
-    unless  => '/usr/bin/defaults read /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0'
+    unless  => ['/usr/sbin/system_profiler SPBluetoothDataType | grep "Connected: Yes"']['/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState | grep -q "0"'],
+    notify  => Exec['bluetooth-disable-if-none']
   }
   exec { 'bluetooth-disable-if-none':
     command     => '/usr/bin/killall -HUP blued',
