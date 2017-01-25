8e89867ace4e925f4d25542d14a59d03375cb74b
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-25 15:39:40 -0500

    Slightly different test

--- manifests/darwin/section2/s2_1_1.pp
+++ manifests/darwin/section2/s2_1_1.pp
@@ -3,7 +3,10 @@
 class cis_config::darwin::section2::s2_1_1 {
   exec { 'bluetooth-device-check':
     command => '/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState -int 0',
-    unless  => ['/usr/sbin/system_profiler SPBluetoothDataType | grep "Connected: Yes"']['/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState | grep -q "0"'],
+    unless  => [
+                  '/usr/sbin/system_profiler SPBluetoothDataType | grep "Connected: Yes"',
+                  '/usr/bin/defaults write /Library/Preferences/com.apple.Bluetooth ControllerPowerState | grep -q "0"'
+                ],
     notify  => Exec['bluetooth-disable-if-none']
   }
   exec { 'bluetooth-disable-if-none':
