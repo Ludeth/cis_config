b66958269b408f63c78d26e608e955a58e5e6d7a
Ludeth <edward.shorrock@cardinalhealth.com>
2017-01-25 21:24:01 -0500

    Removed notify to test command in general

--- manifests/darwin/section2/s2_1_3.pp
+++ manifests/darwin/section2/s2_1_3.pp
@@ -5,11 +5,11 @@ class cis_config::darwin::section2::s2_1_3 {
     command => '/usr/bin/defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"',
     unless  => '/usr/bin/defaults read com.apple.systemuiserver menuExtras | grep -q Bluetooth.menu',
     user    => $::current_user,
-    notify  => Exec['restart-ui-server']
+    #notify  => Exec['restart-ui-server']
   }
 
   exec { 'restart-ui-server':
     command     => '/usr/bin/killall SystemUIServer',
-    refreshonly => true
+    #refreshonly => true
   }
 }
